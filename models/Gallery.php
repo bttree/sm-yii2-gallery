6e482e249a89a1eabd9407eab5d4c816a0a79e92
Sergey <filinsrg@gmail.com>
2017-08-30 10:22:31 +0300

    v1.0.6 fix slug generate

--- models/Gallery.php
+++ models/Gallery.php
@@ -34,6 +34,7 @@ class Gallery extends ActiveRecord
                 'class'         => SluggableBehavior::className(),
                 'attribute'     => 'name',
                 'slugAttribute' => 'slug',
+                'immutable'     => true,
                 'ensureUnique'  => true
             ],
         ];
